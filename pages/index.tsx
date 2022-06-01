4425eaa28d8b57c5fd33ba922465fffbe18746b8
Stationarystation <raulytstation@outlook.com>
2022-06-01 11:11:05 -0400

    feat: Added embed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 
 const Home = () => {
     const currentDate = new Date();
@@ -9,6 +10,14 @@ const Home = () => {
     
     return (
         <>
+        <Head>
+            <title>{difference_in_days} Days since Jungle Inferno</title>
+            <meta property="description" content="Site made by Stationarystation" />
+            <meta property="og:title" content={`${difference_in_days} days since Jungle Inferno`} key="title" />
+            <meta property="og:description" content="Site made by Stationarystation" />
+            <meta property="og:image" content="../public/assets/img/wide.png" />
+        </Head>
+
         <div className="container">
                 <p id="uptitle">Days since Jungle Inferno:</p>
                 <p id="reactiveContent">{difference_in_days}</p>
