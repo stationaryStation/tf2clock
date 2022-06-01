0e1c8192b7c9fd8a3fb30eae689467b4ce0121bd
Stationarystation <raulytstation@outlook.com>
2022-06-01 10:52:43 -0400

    feat: Added Days since Jungle Inferno Counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 const Home = () => {
+    const currentDate = new Date();
+    const JungleInferno = new Date("10/16/2017");
+    const difference_in_time = currentDate.getTime() - JungleInferno.getTime();
+
+    const difference_in_days = Math.floor(difference_in_time / (1000 * 3600 * 24));
+    
     return (
         <>
-            <h1>Hello World!</h1>
+        <div className="container">
+                <p id="uptitle">Days since Jungle Inferno:</p>
+                <p id="reactiveContent">{difference_in_days}</p>
+        </div>
+        <footer>
+            <p id="text">Made by StationaryStation</p>
+            <Link id="link" passHref href="https://stationarystation.vercel.app">Homepage</Link>
+        </footer>
         </>
     )
 }
