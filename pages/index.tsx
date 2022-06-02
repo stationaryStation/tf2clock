338efe5e866d1327b53672cff27b5181b0c5fa33
Raúl (Station) Pérez <raulytstation@Outlook.com>
2022-06-02 11:43:46 +0000

    feat: Add Hours And Milliseconds to timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,48 @@
 import Link from "next/link";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const currentDate = new Date();
-    const JungleInferno = new Date("10/16/2017");
+    const JungleInferno = new Date("10/16/2017 12:00");
     const difference_in_time = currentDate.getTime() - JungleInferno.getTime();
 
     const difference_in_days = Math.floor(difference_in_time / (1000 * 3600 * 24));
+    const difference_in_hours = Math.floor(Math.abs(currentDate.getTime() - JungleInferno.getTime()) /  36e5);
     
+    const [SinceMS, SetSinceMs] = useState<number>(0);
+    const [SinceDays, SetSinceDays] = useState<number>(0);
+    const [SinceHours, SetSinceHours] = useState<number>(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            SetSinceDays(difference_in_days);
+            SetSinceMs(difference_in_time);
+            SetSinceHours(difference_in_hours);
+        }, 5000);
+    }, [difference_in_days,difference_in_time, difference_in_hours])
+
     return (
         <>
         <Head>
             <title>{difference_in_days} Days since Jungle Inferno</title>
             <meta property="description" content="Site made by Stationarystation" />
             <meta property="og:title" content={`${difference_in_days} days since Jungle Inferno`} key="title" />
-            <meta property="og:description" content="Site made by Stationarystation" />
-            <meta property="og:image" content="https://github.com/stationaryStation/tf2clock/blob/master/public/assets/img/wide.png?raw=true" />
+            <meta property="og:description" content={`It has been: ${difference_in_days} days or ${difference_in_hours} hours or ${difference_in_time} ms since jungle inferno released`} key="description" />
+            <meta property="og:image" content="https://github.com/stationaryStation/tf2clock/blob/master/public/assets/img/wide.png?raw=true" key="img" />
         </Head>
 
         <div className="container">
-                <p id="uptitle">Days since Jungle Inferno:</p>
-                <p id="reactiveContent">{difference_in_days}</p>
+            <p id="uptitle">Days since Jungle Inferno:</p>
+            <p id="reactiveContent">{SinceDays} days</p>
+        </div>
+        <div className="container">
+            <p id="uptitle">Hours since Jungle Inferno:</p>
+            <p id="reactiveContent">{SinceHours} hours</p>
+        </div>
+        <div className="container">
+            <p id="uptitle">Milliseconds since Jungle Inferno</p>
+            <p id="reactiveContent">{SinceMS} ms</p>
         </div>
         <footer>
             <p id="text">Made by StationaryStation</p>
