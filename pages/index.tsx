2d3c9601fb861251a55c923d990614bebf4d96b9
Raúl (Station) Pérez <raulytstation@Outlook.com>
2022-06-02 11:58:28 +0000

    feat: Add Minutes and Seconds

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,18 +9,24 @@ const Home = () => {
 
     const difference_in_days = Math.floor(difference_in_time / (1000 * 3600 * 24));
     const difference_in_hours = Math.floor(Math.abs(currentDate.getTime() - JungleInferno.getTime()) /  36e5);
-    
+    const difference_in_minutes = Math.floor((Math.abs(currentDate.getTime() - JungleInferno.getTime()) /  36e5) * 60);
+    const difference_in_seconds = Math.floor((Math.abs(currentDate.getTime() - JungleInferno.getTime()) /  36e5) * 60 * 60);
+
     const [SinceMS, SetSinceMs] = useState<number>(0);
     const [SinceDays, SetSinceDays] = useState<number>(0);
     const [SinceHours, SetSinceHours] = useState<number>(0);
+    const [SinceMinutes, SetSinceMinutes] = useState<number>(0);
+    const [SinceSeconds, SetSinceSeconds] = useState<number>(0);
 
     useEffect(() => {
         setTimeout(() => {
             SetSinceDays(difference_in_days);
             SetSinceMs(difference_in_time);
             SetSinceHours(difference_in_hours);
-        }, 5000);
-    }, [difference_in_days,difference_in_time, difference_in_hours])
+            SetSinceMinutes(difference_in_minutes);
+            SetSinceSeconds(difference_in_seconds);
+        }, 1000);
+    }, [difference_in_days,difference_in_time, difference_in_hours, difference_in_minutes, difference_in_seconds])
 
     return (
         <>
@@ -28,7 +34,7 @@ const Home = () => {
             <title>{difference_in_days} Days since Jungle Inferno</title>
             <meta property="description" content="Site made by Stationarystation" />
             <meta property="og:title" content={`${difference_in_days} days since Jungle Inferno`} key="title" />
-            <meta property="og:description" content={`It has been: ${difference_in_days} days or ${difference_in_hours} hours or ${difference_in_time} ms since jungle inferno released`} key="description" />
+            <meta property="og:description" content={`It has been: ${difference_in_days} days or ${difference_in_hours} hours or ${difference_in_minutes} minutes or ${difference_in_seconds} seconds or ${difference_in_time} ms since jungle inferno released`} key="description" />
             <meta property="og:image" content="https://github.com/stationaryStation/tf2clock/blob/master/public/assets/img/wide.png?raw=true" key="img" />
         </Head>
 
@@ -39,6 +45,14 @@ const Home = () => {
         <div className="container">
             <p id="uptitle">Hours since Jungle Inferno:</p>
             <p id="reactiveContent">{SinceHours} hours</p>
+        </div>
+        <div className="container">
+            <p id="uptitle">Minutes since Jungle Inferno</p>
+            <p id="reactiveContent">{SinceMinutes} minutes</p>
+        </div>
+        <div className="container">
+            <p id="uptitle">Seconds since Jungle Inferno</p>
+            <p id="reactiveContent">{SinceSeconds} seconds</p>
         </div>
         <div className="container">
             <p id="uptitle">Milliseconds since Jungle Inferno</p>
